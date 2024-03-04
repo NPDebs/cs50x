0e097ca6625fd7f8cb2f69dfc2ee418a2272d6c9
NPDebs <deborahudoh02@gmail.com>
2024-03-04 22:30:58 +0100

    Experimenting with booleans
    Signed-off-by: NPDebs <deborahudoh02@gmail.com>

--- hello.c
+++ hello.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+// int main() {
+//     printf("Hello world!\n");
+// }
+
+// Working with booleans.
+int sleep() {
+    bool isSleepy = true;
+    printf("%d\n", (int)isSleepy);
+    printf("Close your eyes, my little munchkin.\n");
+    return 1;
+}
 
 int main() {
-    printf("Hello world!\n");
+    sleep();
+    return 1;
 }
+
